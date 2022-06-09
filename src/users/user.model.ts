edd7c84735ff6047fb838322b565e33a0fbb46d7
Rustanov <akbarrustanov@gmail.com>
2022-06-09 17:21:48 +0600

    Документация swagger подключена

---
+++ src/users/user.model.ts
@@ -0,0 +1,26 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { ApiProperty } from "@nestjs/swagger";
+
+interface userCreationAttr{
+  email: string;
+  password: string;
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User,userCreationAttr > {
+    @ApiProperty({example: '1', description: "Уникальный идентификатор"})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement:true, primaryKey: true})
+    id: number;
+    @ApiProperty({example: 'example@gmail.com', description: "Email"})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+    @ApiProperty({example: '123456', description: "Пароль"})
+    @Column({type: DataType.STRING,allowNull: false})
+    password: string;
+    @ApiProperty({example: 'true', description: "Забанен или нет"})
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+    @ApiProperty({example: 'За жизнь', description: "Нарушение"})
+    @Column({type: DataType.STRING, allowNull: true})
+    banReason: string;
+}
