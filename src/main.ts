d6635e53fe6b0db3d48b67a7ba71f3e093b80491
Rustanov <akbarrustanov@gmail.com>
2022-06-09 14:02:44 +0600

    Файлы конфигурации и настройки закончены

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from "./app.module";
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT, () => console.log(`Server startting ${PORT}`));
+  await app.listen(PORT, () => console.log(`Server starting ${PORT}`));
 }
 start();
